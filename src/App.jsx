6ba7f0a614711705a2981ea97e47ff4ac77d71d1
shivPosted <shivpratap621@gmail.com>
2024-09-13 17:14:34 +0530

    event listener and states in class based components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    this.state = { count: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((cur) => {
+      return { count: cur.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((cur) => {
+      return { count: cur.count + 1 };
+    });
+  }
+  render() {
+    const date = new Date(Date.now());
+    date.setDate(date.getDay() + this.state.count);
+    return (
+      <div className="count">
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{`${date.getDay() + "," + date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear()}`}</span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default Counter;
